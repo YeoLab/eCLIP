8225731e16f0e3632e43bf4e4cb755b19edd6506
Brian Yee <bay001@tscc-1-32.sdsc.edu>
2018-03-24 12:29:06 -0700

    removed unused/deprecated items

--- archived_scripts/update/ecliperrorsfails.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p errors
-grep 'permanentFail' ./eclip-job-log.txt -i -n -B 60 -A 0 > errors/permanentFails.txt
-#grep 'permanentFail' ./ECLIP-JOB-LOG.txt > errors.txt
-
-cat errors/permanentFails.txt
--- archived_scripts/update/eclipexample.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-
-mkdir -p example
-
-cp -ar ${ECLIP_HOME}/init ./example/
